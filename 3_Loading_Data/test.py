eba9911e3f5734d983dd5fe99af46118aebdb9ed
ptsouth97 <ptsouth97@gmail.com>
2020-03-10 21:50:52 -0400

    updated test.py

--- 3_Loading_Data/test.py
+++ 3_Loading_Data/test.py
@@ -4,21 +4,37 @@ import os
 import pandas as pd
 
 where = os.getcwd()
+print("The current working directory is:")
 print(where)
+print()
+
 what = os.listdir(where)
+print("The files is the directory are:")
 print(what)
+print()
+
 path = './assessments/'
-#os.chdir(path)
-folders = os.listdir(path)
+print("Changing directories...")
+os.chdir(path)
+
+now = os.getcwd()
+print("Now the current working directory is:")
+print(now)
+print()
+
+folders = os.listdir(now)
+print("The folders in the current working directory are:")
 print(folders)
+print()
+
 for folder in folders:
 
 	os.chdir(folder)
 
-	df_new = pd.read_csv('new')
+	df_new = pd.read_csv('new.csv')
 	print(df_new)
 
-	df_old = pd.read_csv('old')
+	df_old = pd.read_csv('old.csv')
 	print(df_old)
 
 	os.chdir('..')
